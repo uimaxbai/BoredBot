43e48cfcdfcca8f9a856a5c9b6439fe5ba6a47d7
uimaxbai <61615730+uimaxbai@users.noreply.github.com>
2023-05-18 15:22:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import discord
+import os
+
+bot = discord.Bot()
+
+# we need to limit the guilds for testing purposes
+# so other users wouldn't see the command that we're testing
+
+@bot.command(description="Sends the bot's latency.") # this decorator makes a slash command
+async def ping(ctx): # a slash command will be created with the name "ping"
+    await ctx.respond(f"Pong! Latency is {bot.latency}")
+
+bot.run()
